0d812f9b5dd15fc9b2d9038520d2721e83592892
nmjxblw <zchen386@ucsc.edu>
2023-05-18 17:29:36 -0700

    deleted:    assets/SaveData.json 	renamed:    assets/InGameData.json -> json/InGameData.json 	new file:   json/README.md 	new file:   json/SaveData.json 	modified:   src/globals.js 	modified:   src/scene.js

--- src/globals.js
+++ src/globals.js
@@ -2,43 +2,45 @@
 let Timer = 0;//临时计时器
 let Volume = 0.5;//音量大小，范围:[0-1],float
 let GameOver = false;//用于检测玩家是否死亡
-let saveDataJsonFilePath = './assets/SaveData.json';//存储SaveData.json的路径，string
-let saveData;//玩家临时存档，最终存档存储于localStorage中
+let saveDataJsonFilePath = '../json/SaveData.json';//存储SaveData.json的路径，string
+let saveData = localStorage.getItem('saveData')? JSON.parse(localStorage.getItem('saveData')):{};//玩家临时存档，最终存档存储于localStorage中,JavaScript Object
 let gameData;//游戏数据（关卡数值和其他信息，在'./assets/InGameData.json'中定义以后调用）
 
 //全局函数
 //加载存档
 function loadSaveData() {
-    //1.先检测是否有localStorage，如果有则将数据赋予全局变量saveData
-    saveData = localStorage.getItem("saveData");
+    console.log("已调用loadSaveData()");
     //2.如果没有则从saveData.json中复制
     fetch(saveDataJsonFilePath)
         .then((response) => {
-            response.json();
-        })
-        .then((jsonData) => {
-            //2.如果没有localStorage则从saveData.json中复制数据再赋值
-            if (typeof saveData === 'undefined') {
-                saveData = jsonData;
-                localStorage.setItem('saveData', JSON.stringify(jsonData));
-            }
-            else {
-                //3.假设localStorage存在，检验是否存在异常数据（数据缺失或者undefined）
-                saveDataCheck();
-            }
+            response.json()
+                .then((jsonData) => {
+                    //2.如果没有localStorage则从saveData.json中复制数据再赋值
+                    if (typeof saveData === 'undefined' || !saveData || saveData === {}) {
+                        console.log("检测到saveData为空");
+                        console.log("当前jsonData为:");
+                        console.log(jsonData);
+                        saveData = jsonData;
+                        localStorage.setItem('saveData', JSON.stringify(jsonData));
+                    }
+                    else {
+                        //3.假设localStorage存在，检验是否存在异常数据（数据缺失或者undefined）
+                        saveDataCheck();
+                    }
+                    console.log("当前saveData为:");
+                    console.log(saveData);
+                });
         })
         .catch(error => {
             console.error('发生错误:', error);
         });
-    console.log('saveData:' + saveData);
-    console.log('localStorage:' + localStorage);
-    return;
 }
 
 //写入存档
 function writeSaveData() {
+    console.log("已调用writeSaveData()");
     //1.先检测saveData是否赋值
-    if (typeof saveData === 'undefined') {
+    if (typeof saveData === 'undefined' || !saveData || saveData === {}) {
         console.warn(`正在调用写入存档函数,saveData未赋值!调用加载存档函数为saveData赋值。`);
         //调用加载存档函数为saveData赋值
         loadSaveData();
@@ -48,45 +50,46 @@ function writeSaveData() {
     localStorage.setItem('saveData', JSON.stringify(saveData));
     //检验数据合法性
     saveDataCheck();
-    return;
 }
 
 //检测saveData中数据是否异常（数据缺失或者undefined）
 function saveDataCheck() {
+    console.log("已调用saveDataCheck()");
     var flag = false;
-    var TempSaveData = localStorage.getItem('saveData');
-    //如果TempSaveData存在则将TempSaveData解析为 JavaScript 对象并赋值给localStorageData
-    var localStorageData = TempSaveData ? JSON.parse(TempSaveData) : {};
+    //判断localstorage中是否有‘saveData’键，如果有将数据解析成JavaScript对象并赋值给localStorageData
+    var localStorageData = localStorage.getItem('saveData') ? JSON.parse(localStorage.getItem('saveData')) : {};
 
     fetch(saveDataJsonFilePath)
         .then((response) => {
-            response.json();
-        })
-        .then((jsonData) => {
-            for (var key in jsonData) {
-                if (jsonData.hasOwnProperty(key)) {
-                    if (!(key in localStorageData) || localStorageData[key] === undefined) {
-                        //localStorage存在异常数据 = saveData也存在异常数据，flag为真
-                        flag = true;
-                        localStorageData[key] = jsonData[key];
+            response.json()
+                .then((jsonData) => {
+                    for (var key in jsonData) {
+                        if (jsonData.hasOwnProperty(key)) {
+                            if (!(key in localStorageData) || localStorageData[key] === undefined) {
+                                //localStorage存在异常数据 = saveData也存在异常数据，flag为真
+                                console.warn(`localStorage存在异常数据于键[${key}]`)
+                                flag = true;
+                                localStorageData[key] = jsonData[key];
+                            }
+                        }
                     }
-                }
-            }
-            if (flag) {
-                //回写数据
-                localStorage.setItem('saveData', JSON.stringify(localStorageData));
-                saveData = localStorage.getItem("saveData");
-            }
+                    if (flag) {
+                        //回写数据
+                        console.log("开始回写数据");
+                        localStorage.setItem('saveData', JSON.stringify(localStorageData));
+                        saveData = localStorageData;
+                        console.log("回写完成");
+                    }
+                    console.log(saveData);
+                });
         })
         .catch(error => {
             console.error('发生错误:', error);
         });
-    return;
 }
 
 //删除玩家本地数据并初始化
-function initializeLocal(){
+function initializeLocal() {
     localStorage.clear();
     loadSaveData();
-    return;
 }
--- src/scene.js
+++ src/scene.js
@@ -5,9 +5,9 @@ class testScene extends GameScene {
 
     exCreate() {
         // console.log(this.sceneKey);
-        this.time.delayedCall(5000, () => {
+        /* this.time.delayedCall(5000, () => {
             this.scene.start("testScene2");
-        });
+        }); */
     }
 }
 
@@ -23,14 +23,6 @@ class testScene2 extends GameScene {
         });
     }
 }
-
-
-/* let Timer = 0;//临时计时器
-let Volume = 0.5;//音量大小，范围:[0-1],float
-let GameOver = false;//用于检测玩家是否死亡
-let saveDataJsonFilePath = './assets/SaveData.json';//存储SaveData.json的路径，string
-let saveData;//玩家临时存档，最终存档存储于localStorage中
-let gameData;//游戏数据（关卡数值和其他信息，在'./assets/InGameData.json'中定义以后调用） */
 
 const game = new Phaser.Game({
     type: Phaser.AUTO,
