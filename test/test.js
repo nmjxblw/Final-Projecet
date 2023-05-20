fc36b1cc34793335663e2e79c050129e6e108fb4
nmjxblw <zchen386@ucsc.edu>
2023-05-19 22:19:00 -0700

    modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -22,28 +22,40 @@ class baseTest extends GameScene {
         console.log(distance);
 
         //显示旋转锚点
-        this.add.circle(routatePointX, routatePointY, 10, 0xffffff);
+        let routatePoint = this.add.circle(routatePointX, routatePointY, 10, 0xffffff).setAlpha(0.5);
 
-        //显示初始卡牌的中心
-        this.add.circle(cardRectX, cardRectY, 10, 0xffff00);
-
-        var card = this.add.image(this.cx, this.cy, 'card').setOrigin(0.5); // 设置图片的位置和大小
+        // 设置图片的位置和大小
+        var card = this.add.image(this.cx, this.cy, 'card').setOrigin(0.5);
 
         //测试锚点，用于标记图像的位置
-        let testCicle = this.add.circle(distance * Math.cos(initAngle) + routatePointX, distance * Math.sin(initAngle) + routatePointY, 20, 0x00ff00);
+        let testCicle = this.add.circle(distance * Math.cos(initAngle) + routatePointX, distance * Math.sin(initAngle) + routatePointY, 20, 0x00ff00).setAlpha(0);
 
         this.input.on("pointermove", (pointer) => {
 
             //通过改变旋转锚点的坐标来实现拖动效果
-            if (pointer.x > this.cx + 10) {
-                routatePointX = this.cx + 10;
+            if (pointer.x > this.cx + 20) {
+                routatePointX = this.cx + 20;
             }
-            else if (pointer.x < this.cx - 10) {
-                routatePointX = this.cx + 10;
+            else if (pointer.x < this.cx - 20) {
+                routatePointX = this.cx - 20;
             }
             else {
                 routatePointX = pointer.x;
             }
+
+            routatePoint.x = routatePointX;
+
+            if (pointer.y > this.cy + 10) {
+                routatePointY = this.cy + rectH * 0.75 + 10;
+            }
+            else if (pointer.y < this.cy - 10) {
+                routatePointY = this.cy + rectH * 0.75 - 10;
+            }
+            else {
+                routatePointY = pointer.y + rectH * 0.75;
+            }
+
+            routatePoint.y = routatePointY;
 
             //angleBetweenRotatePoint用于记录鼠标和锚点之间的角度，正数表示鼠标在旋转锚点右侧，负数表示在旋转锚点左侧
             var angleBetweenRotatePoint = Phaser.Math.Angle.Between(routatePointX, routatePointY, pointer.x, pointer.y) + Math.PI / 2;
