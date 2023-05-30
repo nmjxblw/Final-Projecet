b8895a86a64d4ab4eb241d84a3e485530a8168da
nmjxblw <zchen386@ucsc.edu>
2023-05-29 17:39:35 -0700

    修改了json文件的内容

--- src/scene.js
+++ src/scene.js
@@ -332,7 +332,8 @@ const game = new Phaser.Game({
     //scene: [openning, warnning, testScene, testScene2, title, credit],
     //scene: [testlevel],
     //scene: [baseTest1, baseTest2],
-    scene: [baseBattle],
+    //scene: [baseBattle],
+    scene:[thirdFloorLevel1],
     backgroundColor: 0x000000,
     title: "Game",
 });
