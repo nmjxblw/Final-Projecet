a60a40a58db17ebac070ac1b6b90765549e2c701
nmjxblw <zchen386@ucsc.edu>
2023-05-30 10:29:14 -0700

    添加了savedata更新功能

--- src/engine.js
+++ src/engine.js
@@ -19,6 +19,11 @@ class GameScene extends Phaser.Scene {
             }
         }
         this.level = parseInt(key.replace(/[^0-9]/ig, ""));
+        //同时自动更新当前玩家位置信息
+        if(saveData != {}){
+            saveData.currentPosition.floor = this.floor;
+            saveData.currentPosition.level = this.level;
+        }
     }
 
     preload() {
